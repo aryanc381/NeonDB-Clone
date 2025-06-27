fd36c06effcef43d2619987498494c944f67abc3
Aryan Chauhan <135949281+aryanc381@users.noreply.github.com>
2025-06-27 23:30:09 +0530

    Update test.py

--- tools/test.py
+++ tools/test.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from LLM.llm import SarvamChat  
 
 
-llm = SarvamChat(api_key="a43b869d-bdd4-4257-9f93-2753ccc9736d")
+llm = SarvamChat(api_key="SARVAM_API_KEY")
 response = llm.invoke("Tell me about AI in one line")
 ans = response.content.strip('" "')
 print(ans)
